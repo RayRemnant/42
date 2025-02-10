4eddda48c2dde471c966d6ec349151b8d8ae4c04
RayRemnant <49475467+RayRemnant@users.noreply.github.com>
2025-02-10 19:25:50 +0100

    is this ignored?

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -12,19 +12,10 @@ typedef struct s_node
 } t_node;
 
 // Stack operations
-void    sa(t_node **stack);
-void    sb(t_node **stack);
-void    ss(t_node **a, t_node **b);
-void    pa(t_node **a, t_node **b);
-void    pb(t_node **b, t_node **a);
-void    ra(t_node **stack);
-void    rb(t_node **stack);
+void sx(t_node**stack, char stack_char);
+void	px(t_node **dest, t_node **src, char dest_char);
 void 	rx(t_node**stack, char stack_char);
-void    rr(t_node **a, t_node **b);
-void    rra(t_node **stack);
-void    rrb(t_node **stack);
-void rrx(t_node**stack, char stack_char);
-void    rrr(t_node **a, t_node **b);
+void	rrx(t_node**stack, char stack_char);
 
 // Sorting
 void    sort_stack(t_node **a, t_node **b);
